d9211378122465f71908924638d39a9a042af82c
Alexander <kerdevid@gmail.com>
2015-03-27 13:18:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+`go get github.com/gorilla/mux`
+
+build and run and go to `localhost:8080`
+
+routes are: 
+* GET /todos
+* GET /todos/{id}
+* POST /todos
--- repo.go
+++ repo.go
@@ -8,8 +8,8 @@ var todos Todos
 
 // Give us some seed data
 func init() {
-	RepoCreateTodo(Todo{Id: 1, Name: "Write presentation"})
-	RepoCreateTodo(Todo{Id: 2, Name: "Host meetup"})
+	RepoCreateTodo(Todo{Name: "Write presentation"})
+	RepoCreateTodo(Todo{Name: "Host meetup"})
 }
 
 func RepoFindTodo(id int) Todo {
